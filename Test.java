d6a0d4bea9fd69771831f87af1aae68561e187cc
gayomi <gayomiwasana@gmail.com>
2014-02-14 18:18:27 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,9 +1,9 @@
 public class Test{
 
   pulic static void main(String[] args){
-  
+
     //Print your name
     
-    System.out.println("Raandika Isuru Vijayanga");
+    System.out.println("Gayomi Wasana");
   }
 }
