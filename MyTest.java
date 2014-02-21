c518ea32d2c9c42604fa4ed1c86adcd05f86b8e6
Randika Isuru <randikaisuru@gmail.com>
2014-02-21 20:21:18 +0530

    Update MyTest.java
    Create File to use Insert and Dispaly data

--- MyTest.java
+++ MyTest.java
@@ -2,6 +2,19 @@ public class MyTest {
 
 	public static void main(String arg[]){
 	  System.out.println("Hello");
+	  
+	    try{
+            File file = new File("BookList.txt");
+            if(file.exists()){
+            }
+            else{
+            PrintWriter writer = new PrintWriter("BookList.txt", "UTF-8"); // Create a File Name Called BookList.txt
+            }
+        }
+            catch(IOException e){
+            System.out.println("File Not Found");
+        }
+	  
 	}
 
 }
